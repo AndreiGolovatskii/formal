0d1bdc3fe3d80a3230246972d6b5a28146c4ac4e
Андрей Головацкий <andi@Andrejs-MacBook-Pro.local>
2020-10-01 16:31:46 +0300

    refractor, dfa minimization

--- finite_automations.py
+++ finite_automations.py
@@ -1,5 +1,6 @@
-import attr
 from collections import defaultdict, deque
+
+import attr
 
 
 class Eps(object):
@@ -42,17 +43,21 @@ class FiniteAutomation(object):
         return fa
 
     def print(self):
-        for state in self.transition_function.keys():
-            for letter in self.sigma:
-                state_to = self.transition_function[state][letter]
+        for state in self.states:
+            for letter, state_to in self.transition_function[state].items():
                 print(str(state) + "----" + str(letter) + "--->" + str(state_to))
         print("start_state: " + str(self.start_state))
         print("terminal_states: " + str(self.terminal_states))
 
-    def __init__(self, sigma, start_state=None, terminal_states=None):
+    def __init__(self, sigma, states=None, start_state=None, terminal_states=None):
         self.sigma = frozenset(sigma)
         self.start_state = start_state
         self.terminal_states = set(terminal_states) if terminal_states else set()
+        self.states = set(states) if states is not None else set()
+
+        if start_state is not None:
+            self.states.add(start_state)
+        self.states.update(self.terminal_states)
 
     def accept(self, word) -> bool:
         iterator = self.begin()
@@ -62,6 +67,17 @@ class FiniteAutomation(object):
 
     def add_transition(self, state_from, state_to, letter) -> None:
         raise NotImplementedError
+
+    def add_state(self, state):
+        self.states.add(state)
+
+    def add_terminal_state(self, state):
+        self.add_state(state)
+        self.terminal_states.add(state)
+
+    def set_start_state(self, state):
+        self.add_state(state)
+        self.start_state = state
 
     def begin(self) -> Iterator:
         raise NotImplementedError
@@ -92,7 +108,7 @@ class NFA(FiniteAutomation):
             state_queue = deque(states)
             while len(state_queue):
                 new_state = state_queue.popleft()
-                states.update({new_state})
+                states.add(new_state)
                 for state in self.nfa.transition_function[new_state][eps]:
                     if state not in states:
                         state_queue.append(state)
@@ -101,12 +117,14 @@ class NFA(FiniteAutomation):
         def __eq__(self, other):
             return self.nfa == other.nfa and self.states == other.states
 
-    def __init__(self, sigma, start_state=None, terminal_states=None):
-        super().__init__(sigma, start_state, terminal_states)
+    def __init__(self, sigma, states=None, start_state=None, terminal_states=None):
+        super().__init__(sigma, states, start_state, terminal_states)
         self.transition_function = defaultdict(lambda: defaultdict(set))
 
     def add_transition(self, state_from, state_to, letter) -> None:
         assert letter == eps or letter in self.sigma
+        self.states.add(state_to)
+        self.states.add(state_from)
         self.transition_function[state_from][letter].add(state_to)
 
     def begin(self) -> Iterator:
@@ -127,13 +145,15 @@ class DFA(FiniteAutomation):
         def is_terminal(self) -> bool:
             return self.current_state in self.dfa.terminal_states
 
-    def __init__(self, sigma, start_state=None, terminal_states=None):
-        super().__init__(sigma, start_state, terminal_states)
+    def __init__(self, sigma, states=None, start_state=None, terminal_states=None):
+        super().__init__(sigma, states, start_state, terminal_states)
         self.transition_function = defaultdict(dict)
 
     def add_transition(self, state_from, state_to, letter) -> None:
         assert letter in self.sigma
         self.transition_function[state_from][letter] = state_to
+        self.states.add(state_to)
+        self.states.add(state_from)
 
     def begin(self) -> Iterator:
         return DFA.Iterator(self, self.start_state)
@@ -141,17 +161,119 @@ class DFA(FiniteAutomation):
     def renumbered(self):
         mapping = {}
         renumbered_dfa = DFA(self.sigma)
-        for idx, state in enumerate(self.transition_function.keys()):
+        for idx, state in enumerate(self.states):
             mapping[state] = idx
 
-        for idx, state in enumerate(self.transition_function.keys()):
-            for letter in self.sigma:
-                next_state = self.transition_function[state][letter]
-                renumbered_dfa.add_transition(idx, mapping[next_state], letter)
+        for state in self.states:
+            for letter, next_state in self.transition_function[state].items():
+                renumbered_dfa.add_transition(
+                    mapping[state], mapping[next_state], letter
+                )
+
         for terminal_state in self.terminal_states:
-            renumbered_dfa.terminal_states.update({mapping[terminal_state]})
-        renumbered_dfa.start_state = mapping[self.start_state]
+            renumbered_dfa.add_terminal_state(mapping[terminal_state])
+        if self.start_state is not None:
+            renumbered_dfa.set_start_state(mapping[self.start_state])
         return renumbered_dfa
+
+    def reversed_transition_function_(self):
+        tf_reverse = defaultdict(lambda: defaultdict(set))
+        for u in self.states:
+            for letter, v in self.transition_function[u].items():
+                tf_reverse[v][letter].add(u)
+        return tf_reverse
+
+    def reachable_states_(self):
+        reachables = set()
+
+        def dfs(state):
+            if state in reachables:
+                return
+            reachables.add(state)
+            for state_to in self.transition_function[state].values():
+                dfs(state_to)
+
+        dfs(self.start_state)
+        return reachables
+
+    def table_of_unequal_states_(self):
+        queue = deque()
+        marked = defaultdict(lambda: defaultdict(lambda: False))
+        tf_reverse = self.reversed_transition_function_()
+
+        for i in self.states:
+            for j in self.states:
+                if (
+                    not marked[i][j]
+                    and (i in self.terminal_states) != (j in self.terminal_states)
+                ):
+                    marked[i][j] = marked[j][i] = True
+                    queue.append((i, j))
+
+        while len(queue):
+            u, v = queue.pop()
+            for letter in self.sigma:
+                for r in tf_reverse[u][letter]:
+                    for s in tf_reverse[v][letter]:
+                        if not marked[r][s]:
+                            marked[r][s] = marked[s][r] = True
+                            queue.append((r, s))
+        return marked
+
+    def minimized(self):
+        fa = self
+        if not self.is_full():
+            fa = self.completed_to_full()
+
+        unequal = fa.table_of_unequal_states_()
+        reachables = fa.reachable_states_()
+        component = {state: None for state in fa.states}
+
+        cnt = 0
+        for state in fa.states:
+            if state not in reachables:
+                continue
+            if component[state] is None:
+                component[state] = cnt
+                cnt += 1
+                for diff_state in fa.states:
+                    if not unequal[state][diff_state]:
+                        component[diff_state] = component[state]
+        res = DFA(fa.sigma)
+        for state in fa.states:
+            for letter, state_to in fa.transition_function[state].items():
+                res.add_transition(component[state], component[state_to], letter)
+        if fa.start_state is not None:
+            res.set_start_state(component[fa.start_state])
+        for state in fa.terminal_states:
+            res.add_terminal_state(component[state])
+        return res
+
+    def completed_to_full(self):
+        renumbered = self.renumbered()
+        devils_state = -1
+        for letter in renumbered.sigma:
+            renumbered.add_transition(devils_state, devils_state, letter)
+        for state in renumbered.states:
+            for sigma in renumbered.sigma:
+                if state not in renumbered.transition_function:
+                    renumbered.add_transition(state, devils_state, sigma)
+                if sigma not in renumbered.transition_function[state]:
+                    renumbered.add_transition(state, devils_state, sigma)
+
+        assert renumbered.is_full()
+        return renumbered
+
+    def is_full(self):
+        for state in self.states:
+            for letter in self.sigma:
+                if state not in self.transition_function:
+                    return False
+                if letter not in self.transition_function[state]:
+                    return False
+                if self.transition_function[state][letter] not in self.states:
+                    return False
+        return True
 
     @classmethod
     def from_nfa(cls, nfa: NFA):
@@ -162,7 +284,7 @@ class DFA(FiniteAutomation):
         while len(queue):
             state = queue.popleft()
             if state.is_terminal():
-                dfa.terminal_states.add(state)
+                dfa.add_terminal_state(state)
             for letter in dfa.sigma:
                 next_state = state.transition(letter)
                 dfa.add_transition(state, next_state, letter)
